2b0c8ded195ed8dc6cb57f5aaa390e120223030b
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-28 18:14:02 +0300

    Update Task3.java

--- Lesson2/homeWork2/src/com/company/Task3.java
+++ Lesson2/homeWork2/src/com/company/Task3.java
@@ -1,5 +1,5 @@
 package com.company;
-
+//no empty lines!
 ///Напишите программу печати таблицы перевода расстояний из дюймов
 ///в сантиметры для значений длин от 1 до 20 дюймов. 1 дюйм = 2,54 см
 public class Task3 {
@@ -7,6 +7,7 @@ public class Task3 {
     public void function(){
         double cm = 2.54;
         System.out.println("Result: ");
+        //what is 20?
         for(int i = 1; i <= 20; i++){
             System.out.print(i + "inch-" + cm * i + "cm, ");
         }
