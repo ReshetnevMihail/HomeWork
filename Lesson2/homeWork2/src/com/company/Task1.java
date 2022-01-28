7aa4d1b1768ce5d8f8bdc182d878b403962f45e3
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-28 18:11:12 +0300

    Update Task1.java

--- Lesson2/homeWork2/src/com/company/Task1.java
+++ Lesson2/homeWork2/src/com/company/Task1.java
@@ -6,12 +6,14 @@ public class Task1 {
 
     public void function(){
         int ameba = 1;
+        //what is 24? 
         for(int i = 0; i <= 24; i +=3){
             ameba += ameba;
+            //sout goes here. Read the task
         }
         System.out.println("Result: " + ameba);
-
+//delete empty line
     }
-
-
+//delete empty line
+//delete empty line
 }
