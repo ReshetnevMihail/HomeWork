bc06cfe1e4afe5bb736d1e763899400fc6f3505f
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-02-11 19:03:07 +0300

    Update Task1.java

--- lesson3/src/com/company/Task1.java
+++ lesson3/src/com/company/Task1.java
@@ -6,11 +6,14 @@ package com.company;
 // Итак, цвет меняется каждый раз при переходе на соседнюю по стороне ячейку
 
 public class Task1 {
+    //bad method naming.
     public void function(){
+        //9x9 looks strange due to 8x8 
         String[][] array = new String[9][9];
-
+//why from 1? Not from 0 to 8?
         for (int i = 1; i < 9; i++){
             for(int j = 1; j < 9; j++){
+                //hard to read. Think of a better solution
                 if(i % 2 != 0) {
                     if(j % 2 != 0) {
                         array[i][j] = "\u25A0";
