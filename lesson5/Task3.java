26477f6b7dd27dfe4bdb061a336b7f966b655bb6
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-06 23:26:48 +0300

    Update Task3.java

--- lesson5/Task3.java
+++ lesson5/Task3.java
@@ -19,14 +19,15 @@ public class Task3 {
 //запятые не учитываются, как и окночания
 
     public void findWord(Scanner scanner) throws IOException {
-
+//do not use absolute path. It's a very bad practice. 
         String fileName = "D:\\JavaProject\\untitled2\\src\\com\\company\\WarAndPeace.txt";
         String[] array = initWords(scanner);
 
         String book = Files.lines(Paths.get(fileName)).reduce("", String::concat);
-
+//ok for little text. For big - the worst
         int counter = 0;
         for (String j : array) {
+//also I'm not sure about this solution. Think of a better one with while and indexOf method
             for (String i : book.split(" ")) {
                 if (Objects.equals(j, i))
                     counter++;
